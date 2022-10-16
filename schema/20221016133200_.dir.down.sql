b3910942837573f16aa499f9997f451fe91d1df0
Aleksei <katakuxiko@gmail.com>
2022-10-16 19:46:20 +0600

    base db

---
+++ schema/20221016133200_.dir.down.sql
@@ -0,0 +1,9 @@
+DROP table users_list;
+DROP table books_list;
+DROP table users;
+DROP table list_items;
+DROP table books_item;
+
+
+
+
---
+++ schema/20221016133200_.dir.up.sql
@@ -0,0 +1,27 @@
+create table users(
+	id serial not null unique ,
+	name varchar(255) not null ,
+	username varchar(255) not null ,
+	passwordHash varchar(255) not null 
+);
+create table books_list (
+	id serial not null unique ,
+	title varchar(255) not null,
+	description varchar(255)
+);
+create table users_list (
+	id serial not null unique ,
+	user_id int references users(id) on delete cascade not null,
+	list_id int references books_list(id) on delete cascade not null
+);
+create table books_items(
+	id serial not null unique ,
+	title varchar(255) not null,
+	description varchar(255),
+	done bool not null default false
+);
+create table list_items (
+	id serial not null unique ,
+	item_id int references books_items(id) on delete cascade not null,
+	list_id int references books_list(id) on delete cascade not null
+);
